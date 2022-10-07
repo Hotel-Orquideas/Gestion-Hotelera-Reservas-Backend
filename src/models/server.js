3fa2067e725e2f789ccbfc4d07ba15c10b526f52
AndresCrg <36751170+AndresCrg@users.noreply.github.com>
2022-10-07 01:21:37 -0500

    PHO-17 Se instla cors para permitir las peticiones a la API

--- src/models/server.js
+++ src/models/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 class Server {
 	constructor() {
@@ -11,6 +12,7 @@ class Server {
 
 	middleware() {
 		this.app.use(express.json());
+		this.app.use(cors());
 	}
 
 	routes() {
