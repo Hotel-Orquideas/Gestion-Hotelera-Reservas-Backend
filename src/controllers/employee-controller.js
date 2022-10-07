492cb97789cb3238191c8c9a81f35d09643f701b
AndresCrg <36751170+AndresCrg@users.noreply.github.com>
2022-10-07 01:05:38 -0500

    PHO-17 Se crea la funcionalidad de crear empleado. Se hacen pruebas en postman y funcina. Se encuentra el error de que la relacion con roles es de 1:n, falta corregirlo

--- src/controllers/employee-controller.js
+++ src/controllers/employee-controller.js
@@ -1,4 +1,43 @@
-const createEmployee = (req, res) => {};
+const { PrismaClient } = require('@prisma/client');
+const { request, response } = require('express');
+
+const prisma = new PrismaClient();
+
+const createEmployee = async (req = request, res = response) => {
+	const { name, lastName, typeDocument, document, genre, birthdate, phoneNumber, email, bloodType, position } = req.body;
+	const result = await prisma.employee.create({
+		data: {
+			position,
+			person: {
+				create: {
+					name,
+					lastName,
+					typeDocument,
+					document,
+					genre,
+					birthdate,
+					phoneNumber,
+					email,
+					bloodType,
+				},
+			},
+			role: {
+				create: {
+					name: 'Employee',
+				},
+			},
+			hotel: {
+				connect: { id: 1 },
+			},
+		},
+		include: { person: true },
+	});
+	res.json({
+		msg: 'Employee create sucessfull!',
+		result,
+	});
+	console.log(result);
+};
 
 const getEmployee = (req, res) => {
 	res.send('Hola mundo');
